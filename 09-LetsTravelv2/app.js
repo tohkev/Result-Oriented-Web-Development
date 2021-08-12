788fb11f593b18e72eb49e718c23355e06712fd4
Kevin <tohkev@gmail.com>
2021-08-11 21:27:02 -0700

    Updated LT2 app.js
    - added mongoose to access mongodb database
    - created schema that will be used for creating our travel posts
    - tested adding new entry to mongodb

--- 09-LetsTravelv2/app.js
+++ 09-LetsTravelv2/app.js
@@ -1,9 +1,29 @@
 let express = require('express');
 let app = express();
+let mongoose = require('mongoose');
+let Post = require('./models/post').Post;
+
+mongoose.connect('mongodb://localhost/travels').then(() => {
+    console.log('Connected to MongoDB')
+}).catch(() => {
+    console.log('Something went wrong')
+})
+
+let post1 = new Post({
+    id: 2,
+    title: 'Statue of Liberty',
+    date: new Date(),
+    description: 'Some Description',
+    text: 'Some Text',
+    country: 'USA',
+    imageURL: '/images/1.jpg'
+});
+
+post1.save().then(() => {
+    console.log('Country has been saved');
+});
 
 app.use(express.static('public'));
-
-
 
 app.listen(3000, () => {
     console.log('Listening 3000...');
